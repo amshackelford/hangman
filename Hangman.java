2b4de4c4804c3e7572672a4ea8fe200879edea8d
Anthony Shackelford <anthony.shackelford@hotmail.com>
2019-01-27 01:39:04 +0000

    initial commit

---
+++ Hangman.java
@@ -0,0 +1,19 @@
+// Main starts game
+public class Hangman {
+
+  public static void main(String[] args) {
+    // Your incredible code goes here...
+    if (args.length == 0) {
+      System.out.println("Usage:  java Hangman <answer>");
+      System.err.println("answer is required");
+      System.exit(1);
+    }
+    Game game = new Game(args[0]);
+    Prompter prompter = new Prompter(game);
+    while (game.getRemainingTries() > 0 && !game.isWon()) {
+      prompter.displayProgress();
+      prompter.promptForGuess();
+    }
+    prompter.displayOutcome();
+  }
+}
