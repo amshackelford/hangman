d2f7f5a122b6c1f274b1cac66ca3b4ba21a86aba
Anthony Shackelford <anthony.shackelford@hotmail.com>
2019-01-29 18:02:50 +0000

    allow user defined answer

--- Hangman.java
+++ Hangman.java
@@ -3,17 +3,22 @@ public class Hangman {
 
   public static void main(String[] args) {
     // Your incredible code goes here...
-    /*if (args.length == 0) {
-      System.out.println("Usage:  java Hangman <answer>");
-      System.err.println("answer is required");
-      System.exit(1);
-    }*/
-    Game game = new Game(WordBank.getRandomWord());
-    Prompter prompter = new Prompter(game);
-    while (game.getRemainingTries() > 0 && !game.isWon()) {
-      prompter.displayProgress();
-      prompter.promptForGuess();
+    if (args.length == 0) {
+      Game game = new Game(WordBank.getRandomWord());
+      Prompter prompter = new Prompter(game);
+      while (game.getRemainingTries() > 0 && !game.isWon()) {
+        prompter.displayProgress();
+        prompter.promptForGuess();
+      }
+      prompter.displayOutcome();
+    } else {
+      Game game = new Game(args[0]);
+      Prompter prompter = new Prompter(game);
+      while (game.getRemainingTries() > 0 && !game.isWon()) {
+        prompter.displayProgress();
+        prompter.promptForGuess();
+      }
+      prompter.displayOutcome();
     }
-    prompter.displayOutcome();
   }
 }
